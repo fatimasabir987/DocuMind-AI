a44107eeeb7fd4fca0bf5367c056e526d8bff9d6
fatimasabir987 <fatimasabir979@gmail.com>
2026-05-04 02:51:04 +0500

    Fix __init__.py conflict

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 import os
 
@@ -12,19 +11,4 @@ def create_app():
     from .routes import main
     app.register_blueprint(main)
 
-=======
-from flask import Flask
-import os
-
-def create_app():
-    app = Flask(__name__, template_folder='../templates')
-    app.config['UPLOAD_FOLDER'] = 'uploads'
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
-    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
-    from .routes import main
-    app.register_blueprint(main)
-
->>>>>>> 5599ccfce10a329c37abe2f1e767e1ec9720d3aa
     return app
