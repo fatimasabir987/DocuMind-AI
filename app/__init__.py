1ce595790ba7ba09d033ce7e0082a653da1c7d97
fatimasabir987 <fatimasabir979@gmail.com>
2026-05-03 22:12:03 +0500

    Fix: remove numpy dependency

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+def create_app():
+    app = Flask(__name__, template_folder='../templates')
+    app.config['UPLOAD_FOLDER'] = 'uploads'
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
