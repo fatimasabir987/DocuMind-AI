10cfa7c8c9ec35bc9e76cdc3a8c79ea5c2f94b06
fatimasabir987 <fatimasabir979@gmail.com>
2026-05-03 22:48:38 +0500

    Fix merge conflict in run.py

--- run.py
+++ run.py
@@ -1,14 +1,12 @@
-<<<<<<< HEAD
 from app import create_app
+import webbrowser
+import threading
 
 app = create_app()
 
-if __name__ == '__main__':
-=======
-from app import create_app
-
-app = create_app()
+def open_browser():
+    webbrowser.open('http://127.0.0.1:5000')
 
 if __name__ == '__main__':
->>>>>>> 5599ccfce10a329c37abe2f1e767e1ec9720d3aa
+    threading.Timer(1.5, open_browser).start()
     app.run(debug=True)
